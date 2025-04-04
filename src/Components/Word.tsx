7ddceebdaa24e0011b16bd45aa5aca29460a11d8
Steve <ojeladelerin2020@gmail.com>
2025-04-04 22:45:17 +0100

    hint prank

--- src/Components/Word.tsx
+++ src/Components/Word.tsx
@@ -9,9 +9,9 @@ const Word = ({wordProps}: {wordProps: WordProperties[]}) => {
   return (
     <section className='flex gap-2 flex-col items-center w-[50vw] select-none mt-5'>
       {/* <p className='font-extrabold text-xl text-[1.5rem]'> Word </p> */}
-      {/* <div className='font-bold w-full flex justify-center items-center text-2xl'>
+      <div className='font-bold w-full flex justify-center items-center text-2xl'>
         Hint: {wordProps[0].letter}
-      </div> */}
+      </div>
       <div className='flex flex-row gap-2'>
        {wordProps.map((letterObj, index) => {
           return  <Letter key={index} picked = {letterObj.picked}>{letterObj.letter}</Letter>
