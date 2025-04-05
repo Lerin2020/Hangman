bc7d12502ffe7dd7cd23442b96e2495b35af0115
Steve <ojeladelerin2020@gmail.com>
2025-04-05 10:51:56 +0100

    svg edit

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -5,9 +5,9 @@ const Loading = () => {
         
   <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg" className="w-20 h-20">
     <circle className="spin2" cx="400" cy="400" fill="none"
-      r="200" stroke-width="50" stroke="#0C120C"
-      stroke-dasharray="700 1400"
-      stroke-linecap="round" />
+      r="200" strokeWidth="50" stroke="#0C120C"
+      strokeDasharray="700 1400"
+      strokeLinecap="round" />
   </svg>
   </div>
   )
